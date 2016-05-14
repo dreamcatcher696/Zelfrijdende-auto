a07c82f6dd81c037c5eb5455939c327bee2412ea
MaximAelterman <maximaelterman@gmail.com>
2016-05-14 15:42:22 +0200

    autonoom rijden functie

---
+++ autonoom.c
@@ -0,0 +1,283 @@
+//		Inlcludes/Defines		//
+#include <avr/io.h>
+#define F_CPU 8000000
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#define UITGANGV	PB6
+#define UITGANGA	PB7
+#define UITGANGL	PD5
+#define UITGANGR	PD6
+#define INGANGV		PC3
+#define INGANGA		PC2
+#define INGANGL		PC1
+#define INGANGR		PC0
+#define ECHO_F		PD2
+#define ECHO_L		PD1
+#define ECHO_R		PC5
+#define TRIGGER_F	PD4
+#define TRIGGER_L	PD7
+#define TRIGGER_R	PB0
+#define AFSTAND		20
+//		Aanmaken snel geheugen om stop te schrijven		//
+volatile uint8_t	stop_F = 0;			//als stop == 0 dan geen object gedetecteerd
+volatile uint8_t	stop_L = 0;
+volatile uint8_t	stop_R = 0;
+volatile uint8_t	afstand_L = 0;
+volatile uint8_t	afstand_R = 0;
+volatile uint8_t	richting = 0;		//0 - vooruit	1 - links	2 - rechts	3 - achteruit
+//		Functie declaraties		//
+void timer1Start();
+void timer1Stop();
+void timer0Start();
+void timer0Stop();
+void timer2Start();
+void timer2Stop();
+//		start main		//
+int main(void)
+{
+	//		poort declaratie(IO)		//
+	DDRB |=  (1<<UITGANGV);
+	DDRB |=  (1<<UITGANGA);
+	DDRD |=  (1<<UITGANGL);
+	DDRD |=  (1<<UITGANGR);
+	DDRC &= ~(1<<INGANGV);
+	DDRC &= ~(1<<INGANGA);
+	DDRC &= ~(1<<INGANGL);
+	DDRC &= ~(1<<INGANGR);
+	DDRD &= ~(1<<ECHO_F);
+	DDRD &= ~(1<<ECHO_L);
+	DDRC &= ~(1<<ECHO_R);
+	DDRD |=  (1<<TRIGGER_F);
+	DDRD |=	 (1<<TRIGGER_L);
+	DDRB |=  (1<<TRIGGER_R);
+	
+	//interrupt initialiseren
+	
+	sei();				//global interrupts enable (SREG interrupts 1)
+	EICRA |= (1<<ISC01)| (1<<ISC00);   //rising edge interrupt controle (ISC00 op 0 en isc 01 op 1 voor falling)
+	EIMSK |= (1<<INT0);					//enablen van external interrupt 0
+	
+									//PCICR voor enabken van pin change interrupt[23-16] enz.
+									//PCMSK voor enablen van apparte interrupts?
+									//PCIFR bevat de interrupt flags
+								
+	PCICR |= (1<<PCIE2);
+	PCICR |= (1<<PCIE1);	//enablen van pin change interrupt 23tot 6 en 14 tot 8
+	PCMSK2 |=(1<<PCINT17);				//enablen van pin change interrupt 17
+	PCMSK1 |=(1<<PCINT13);				//enablen van pin change interrupt 13
+	
+	TCNT0 = 0;					//timer register op 0 zetten
+	TCNT1 = 0;							//TCCR0B voor prescaler settings(CS02 op 1 voor 256 prescaler=>max range = 1.4m)
+	TCNT2 = 0;							//TCCR2B ook op 256 prescaler => timer puls om de (8000000/256)==>1/31250==> om de 32 microseconden==> 32*255 = max lengte van object ==>/58==>1.4m
+								//TCCR2B CS22=> 1 CS21 => 1 CS20==> 0
+	OCR1A = 38000;
+	TCCR1B |= (1<<WGM12);		//WGM12= ctc mode , compraen met ocr1A ...CS11 is 8 bit prescaler => counter om de 1us
+	TCNT1 = 0;					//timer register op 0 zetten
+    while(1)
+    {
+		//trigger signaal sturen
+		PORTD &= ~(1<<TRIGGER_F);
+		PORTD |= (1<<TRIGGER_F);
+		_delay_us(10);
+		PORTD &= ~(1<<TRIGGER_F);
+		//eind trigger signaal
+		
+		if(stop_F == 0)
+		{
+			//vooruit rijden
+			richting = 0;
+			PORTD &=~(1<<UITGANGL);					//wielen centreren
+			PORTD &=~(1<<UITGANGR);
+			
+			PORTB |=(1<<UITGANGV);					//gas geven
+			_delay_ms(100);							//niet te snel rijden (auto niet kapot rijden tijdens tests!)
+			PORTB &=~(1<<UITGANGV);
+			_delay_ms(100);
+		}
+		else
+		{
+			//trigger signaal LEFT sturen
+			PORTD &= ~(1<<TRIGGER_L);
+			PORTD |= (1<<TRIGGER_L);
+			_delay_us(10);
+			PORTD &= ~(1<<TRIGGER_L);
+			//eind trigger signaal LEFT
+			
+			//trigger signaal RIGHT sturen
+			PORTB &= ~(1<<TRIGGER_R);
+			PORTB |= (1<<TRIGGER_R);
+			_delay_us(10);
+			PORTB &= ~(1<<TRIGGER_R);
+			//eind trigger RIGHT signaal
+			
+			_delay_ms(30);							//wachten op echo signalen
+			
+			if(richting == 0)
+			{
+				if((afstand_R >= afstand_L) && (stop_R == 0))
+				{
+					//rechts rijden
+					richting = 2;
+					
+								
+				}
+				else if(stop_L == 0)
+				{
+					//links rijden
+					richting = 1;
+				}
+				else
+				{
+					//achteruit rijden
+					richting = 3;
+				}
+			}
+			else
+			{
+				if(richting == 1)		//links
+				{
+					PORTD |=(1<<UITGANGL);					//wielen naar links draaien
+					
+					PORTB |=(1<<UITGANGV);					//gas geven
+					_delay_ms(100);							
+					PORTB &=~(1<<UITGANGV);
+					_delay_ms(100);
+				}
+				if(richting == 2)		//rechts
+				{
+					PORTD |=(1<<UITGANGR);					//wielen naar rechts draaien -> blijven in deze positie tot de weg voor de auto vrij is
+					
+					PORTB |=(1<<UITGANGV);					//gas geven
+					_delay_ms(100);							
+					PORTB &=~(1<<UITGANGV);
+					_delay_ms(100);
+				}
+				if(richting == 3)		//achteruit
+				{
+					PORTD &=~(1<<UITGANGL);					//wielen centreren
+					PORTD &=~(1<<UITGANGR);
+					
+					PORTB |=(1<<UITGANGA);					//achteruit rijden
+					_delay_ms(100);
+					PORTB &=~(1<<UITGANGA);
+					_delay_ms(100);
+				}
+			}
+		}
+    }
+}
+ISR(INT0_vect)
+{
+	static uint8_t status1 = 1; //als status==1 dan is de pin hoog. en omgekeerd
+	static uint16_t timerwaarde1  =0;
+	if(status1==1)
+	{
+		timer1Start();
+		status1 = 0;
+	}
+	else
+	{
+		timer1Stop();
+		status1 = 1;
+		timerwaarde1 = TCNT1 / 58;
+		if(timerwaarde1 <= 20)
+		{
+				stop_F = 1;
+		}
+		else
+		{
+				stop_F = 0;
+		}
+	}
+}
+ISR(PCINT1_vect)
+{
+	static uint8_t status0 = 1; //als status==1 dan is de pin hoog. en omgekeerd
+	static uint16_t timerwaarde0  =0;
+	if(status0==1)
+	{
+		timer0Start();
+		status0 = 0;
+	}
+	else
+	{
+		timer0Stop();
+		status0 = 1;
+		//timerwaarde1 = TCNT0 / 1856;		//1 timer waarde om de 32 us. dus delen door 32 om 1 us te hebben, dan delen door 58 om naar cm te gaan
+		timerwaarde0 = TCNT0 ;
+		afstand_R = timerwaarde0;
+		if(timerwaarde0 <= 40)
+		{
+			stop_R = 1;
+		}
+		else
+		{
+			stop_R = 0;
+		}
+	}
+}
+ISR(PCINT2_vect)
+{
+	static uint8_t status2 = 1; //als status==1 dan is de pin hoog. en omgekeerd
+	static uint16_t timerwaarde2  =0;
+	if(status2==1)
+	{
+		timer2Start();
+		status2 = 0;
+	}
+	else
+	{
+		timer2Stop();
+		status2 = 1;
+		timerwaarde2 = TCNT2;
+		afstand_L = timerwaarde2;
+		if(timerwaarde2 <= 40)
+		{
+			stop_L = 1;
+		}
+		else
+		{
+			stop_L = 0;	
+		}
+	}
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	//niets gemeten
+}
+
+void timer1Start()
+{
+	TCNT1 = 0;
+	EICRA &= ~(1<<ISC00);	//volgende keer op faling edge interrupten
+	TCCR1B |= (1<<CS11);//prescaler(8) zetten = `klok starten	
+}
+void timer1Stop()
+{
+	EICRA |=(1<<ISC00);		//volgende keer op rising edge interrupten
+	TCCR1B &= ~(1<<CS11);	
+}
+
+void timer0Start()
+{
+	TCNT0 = 0;
+	TCCR0B |= (1<<CS02);
+}
+
+void timer0Stop()
+{
+	TCCR0B &= ~(1<<CS02);
+}
+
+void timer2Start()
+{
+	TCNT2 = 0;
+	TCCR2B |= (1<<CS22);
+	TCCR2B |= (1<<CS21);
+}
+void timer2Stop()
+{
+	TCCR2B &= ~(1<<CS22);
+	TCCR2B &= ~(1<<CS21);
+}
